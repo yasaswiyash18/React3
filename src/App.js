be2e70ea27534a223d59c11e2a9f9de8f7775a7d
Yasaswi <yasaswiyash18@gmail.com>
2023-10-13 15:21:28 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from 'react';
 
+import './App.css';
 function App() {
+  const [quote, setQuote] = useState("Hello");
+  const [author, setAuthor] = useState("World");
+  //http://api.quotable.io/random
+  
+  useEffect(() => {
+    fetch("http://api.quotable.io/random")
+    .then(res => res.json())
+    .then(
+      (quote) => {
+        setQuote(quote.content);
+        setAuthor(quote.author);
+      }
+    )
+  },[]);
+  let fetchNewQuote = () => {
+    fetch("http://api.quotable.io/random")
+    .then(res => res.json())
+    .then(
+      (quote) => {
+        setQuote(quote.content);
+        setAuthor(quote.author);
+      }
+    ) 
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className= "App" >
+      <div className="quote" >
+        <h2>{quote}</h2>
+        <small>-{author}-</small>
+      </div>
+      <button className="btn" onClick={fetchNewQuote}>Generate New Quote</button>
     </div>
   );
 }
